303eaaba354409e602a538b0eb25274525562ab6
Niv Suroghon <nsuroghon@gmail.com>
2020-12-03 16:09:57 -0500

    adding license badge

--- index.js
+++ index.js
@@ -65,6 +65,8 @@ const questions = inquirer
     `
 # ${data.title}
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 ## Table of Contents
 * [Description](#description)
 * [Installation Guide](#installation)
