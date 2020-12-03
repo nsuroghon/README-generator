a5e1253fd3892d3f43251e401fbf97db5c089175
Niv Suroghon <nsuroghon@gmail.com>
2020-12-03 14:52:22 -0500

    completed markdown and tested readme preview for functionality

--- index.js
+++ index.js
@@ -1,63 +1,107 @@
-const inquirer = require('inquirer');
-//const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const questions = inquirer.prompt([
+const questions = inquirer
+  .prompt([
     //title
-        {
-            type: "input",
-            message: "Enter a project title",
-            name: "title"
-        },
+    {
+      type: "input",
+      message: "Enter a project title",
+      name: "title",
+    },
     //description
-        {
-            type: "input",
-            message: "Enter a project description",
-            name: "description"
-        },
+    {
+      type: "input",
+      message: "Enter a project description",
+      name: "description",
+    },
     //installation instructions
-         {
-            type: "input",
-            message: "What are this project's installation instructions?",
-            name: "installation"
-        },
-     //usage information
-     {
-        type: "input",
-        message: "What is the usage purpose of this project?",
-        name: "usage"
+    {
+      type: "input",
+      message: "What are this project's installation instructions?",
+      name: "installation",
     },
-     //contribution guidelines
-     {
-        type: "input",
-        message: "Who contributed in creating this project?",
-        name: "contribution"
+    //usage information
+    {
+      type: "input",
+      message: "What is the usage purpose of this project?",
+      name: "usage",
     },
-     //test instructions
-     {
-        type: "input",
-        message: "What are the test instructions?",
-        name: "tests"
+    //contribution guidelines
+    {
+      type: "input",
+      message: "Who contributed in creating this project?",
+      name: "contribution",
+    },
+    //test instructions
+    {
+      type: "input",
+      message: "What are the test instructions?",
+      name: "tests",
     },
     //License (DROPDOWN OPTION)
     {
-        type: "checkbox", 
-        message: "Select a license",
-        choices: [
-            "MIT",
-            "Apache"
-        ],
-        name: "license"
+      type: "checkbox",
+      message: "Select a license",
+      choices: ["MIT", "Apache"],
+      name: "license",
     },
     //username
     {
-        type: "input",
-        message: "Enter your github username?",
-        name: "username"
+      type: "input",
+      message: "Enter your github username?",
+      name: "username",
     },
     //email address
     {
-        type: "input",
-        message: "Enter your email address",
-        name: "email"
+      type: "input",
+      message: "Enter your email address",
+      name: "email",
     },
-    ])
+  ])
+  .then(function (data) {
+    fs.writeFile("readme.md",
+    // pass on info to file
+    `
+    # ${data.title}
+
+    ## Table of Contents
+    * [App Description](#description)
+    * [Installation Guide](#installation)
+    * [Usage Information](#usage)
+    * [Contributions](#contribution)
+    * [Application Test](#tests)
+    * [License](#license)
+
+    ## Description
+    ${data.description}
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## Contribution
+    ${data.contribution}
+
+    ## Test
+    ${data.tests}
+
+    ## License
+    ${data.license}
+
+    ## More Info
+    Github: ${data.username}
+    Email: ${data.email}
+    `,
+    //callback function
+     function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
+  });
+
+  //formatted with extension prettier, cmd shift p 
+
+
---
+++ readme.md
@@ -0,0 +1,33 @@
+
+    # Readme Generator
+
+    ## Table of Contents
+    * [App Description](#description)
+    * [Installation Guide](#installation)
+    * [Usage Information](#usage)
+    * [Contributions](#contribution)
+    * [Application Test](#tests)
+    * [License](#license)
+
+    ## Description
+    1
+
+    ## Installation
+    2
+
+    ## Usage
+    3
+
+    ## Contribution
+    4
+
+    ## Test
+    
+
+    ## License
+    MIT
+
+    ## More Info
+    Github: 5
+    Email: 6
+    
